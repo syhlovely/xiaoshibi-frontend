4529f9d647a6fd80886f2ff9ac8a990892a7e06e
Administrator <1584442935@qq.com>
2023-07-28 22:07:43 +0800

    修改框架模板 登录功能开发

--- config/config.ts
+++ config/config.ts
@@ -73,7 +73,7 @@ export default defineConfig({
    * @name layout 插件
    * @doc https://umijs.org/docs/max/layout-menu
    */
-  title: 'Ant Design Pro',
+  title: '鱼智能 BI',
   layout: {
     locale: true,
     ...defaultSettings,
@@ -130,7 +130,7 @@ export default defineConfig({
     {
       requestLibPath: "import { request } from '@umijs/max'",
       // 或者使用在线的版本
-      schemaPath: "http://localhost:12345/api/v2/api-docs",
+      schemaPath: "http://localhost:8080/api/v2/api-docs",
       projectName: 'yubi',
       // schemaPath: join(__dirname, 'oneapi.json'),
       mock: false,
